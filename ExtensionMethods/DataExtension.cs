cf2b69af831e59871449f1ae182247e380edabff
Romario Angelo <romario-etical@hotmail.com>
2021-05-12 02:04:24 +0100

    Add project files.

---
+++ ExtensionMethods/DataExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    public static class DataExtension
+    {
+        public static string PrimeiroFormato(this DateTime data)
+        {
+            return data.ToString("dd/MM/yyyy");
+        }
+        public static string SegundoFormato(this DateTime data)
+        {
+            return data.ToString("dd-MM-yyyy");
+        }
+        public static string TerceiroFormato(this DateTime data)
+        {
+            return data.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+    public static class StringExtension
+    {
+        public static string BoasVindas(this string texto)
+        {
+            return "Seja bem-vindo ao meu canal";
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //12/05/2021
+            //12-05-2021
+            //2021-05-12
+            string bv = string.Empty;
+            Console.WriteLine(bv.BoasVindas());
+            Console.WriteLine($"Formato 1: {DateTime.Now.PrimeiroFormato()}");
+            Console.WriteLine($"Formato 2: {DateTime.Now.SegundoFormato()}");
+            Console.WriteLine($"Formato 1: {DateTime.Now.TerceiroFormato()}");
+
+        }
+
+    }
+}
